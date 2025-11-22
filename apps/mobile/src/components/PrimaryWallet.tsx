dd7403f34a1fd7c1c3de0ba98011d2b67d723106
HarshK97 <harshkapse1234@gmail.com>
2025-11-22 12:25:00 +0530

    Added Wallets

---
+++ apps/mobile/src/components/PrimaryWallet.tsx
@@ -0,0 +1,49 @@
+import { IonCard, IonCardContent, IonIcon, isPlatform } from "@ionic/react";
+import { trendingUpOutline, walletOutline } from "ionicons/icons";
+
+const PrimaryWallet = () => {
+  const isAndroid = isPlatform("android");
+  const isIOS = isPlatform("ios");
+
+  return (
+    <div className="space-y-6 px-6 py-4 text-white">
+      <IonCard className="primary-card">
+        <IonCardContent>
+          <div
+            style={{
+              padding: isAndroid ? "12px" : "0px",
+              marginBottom: isIOS ? "0" : "12px",
+            }}
+            className="mt-2 flex flex-col gap-6"
+          >
+            <div className="flex flex-row items-center justify-between gap-4">
+              <div className="flex items-center rounded-xl border border-white/20 bg-white/15 p-3 text-2xl text-white backdrop-blur-xl">
+                <IonIcon icon={walletOutline} />
+              </div>
+
+              <div className="flex flex-col items-end">
+                <span className="text-sm font-bold text-gray-200">
+                  PRIMARY WALLET
+                </span>
+                <span className="text-xs font-bold text-gray-200">
+                  ****8821
+                </span>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-start gap-2 text-white">
+              <span className="text-4xl font-bold">₹4,250</span>
+
+              <div className="flex items-center gap-2 rounded-xl border border-white/20 bg-white/15 px-2 py-1 text-xs backdrop-blur-xl">
+                <IonIcon icon={trendingUpOutline} />
+                <span>UPI Linked</span>
+              </div>
+            </div>
+          </div>
+        </IonCardContent>
+      </IonCard>
+    </div>
+  );
+};
+
+export default PrimaryWallet;
--- apps/mobile/src/pages/Home.tsx
+++ apps/mobile/src/pages/Home.tsx
@@ -1,30 +1,51 @@
 import { IonContent, IonPage, IonHeader } from "@ionic/react";
 import React, { useState } from "react";
 import Welcome from "../components/Welcome";
+import PrimaryWallet from "../components/PrimaryWallet";
+import SavingsWallet from "../components/SavingsWallet";
 
 const Home: React.FC = () => {
   const [activeWallet, setActiveWallet] = useState<"Primary" | "Savings">(
     "Primary",
   );
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const handleScroll = (event: CustomEvent) => {
+    const scrollTop = event.detail.scrollTop;
+    if (scrollTop > 50 && showWelcome) {
+      setShowWelcome(false);
+    } else if (scrollTop <= 50 && !showWelcome) {
+      setShowWelcome(true);
+    }
+  };
 
   return (
     <IonPage>
       <IonHeader>
-        <Welcome />
-        <div className="ion-margin relative flex flex-row rounded-4xl bg-gray-800 px-2 py-2 text-center text-white">
+        <div
+          className={`relative z-10 overflow-hidden transition-all duration-300`}
+          style={{ height: showWelcome ? "160px" : "0px" }}
+        >
+          <Welcome />
+        </div>
+
+        <div
+          className={
+            "ion-margin relative z-10 flex flex-row rounded-4xl bg-gray-800 px-2 py-2 text-center text-white transition-all duration-300"
+          }
+          style={{ marginTop: showWelcome ? "16px" : "70px" }}
+        >
           <div
-            className={`absolute top-2 bottom-2 w-[calc(50%-4px)] rounded-4xl bg-(--ion-color-primary) transition-all duration-300 ${
+            className={`absolute top-2 bottom-2 w-[calc(50%-10px)] rounded-4xl bg-(--ion-color-primary) transition-all duration-300 ${
               activeWallet === "Primary" ? "left-2" : "left-[calc(50%+2px)]"
             }`}
           />
-
           <div
             onClick={() => setActiveWallet("Primary")}
             className="z-10 flex-1 px-6 py-2"
           >
             Primary
           </div>
-
           <div
             onClick={() => setActiveWallet("Savings")}
             className="z-10 flex-1 px-6 py-2"
@@ -34,8 +55,9 @@ const Home: React.FC = () => {
         </div>
       </IonHeader>
 
-      <IonContent className="">
-        <h1 className="text-white">Content goes here</h1>
+      <IonContent scrollEvents={true} onIonScroll={handleScroll} className="">
+        {activeWallet === "Primary" && <PrimaryWallet />}
+        {activeWallet === "Savings" && <SavingsWallet />}
       </IonContent>
     </IonPage>
   );
