5f729cd09e478d3d824c96a78caffa8c7e5ed4f4
HarshK97 <harshkapse1234@gmail.com>
2025-11-24 15:14:45 +0530

    Merged Feature/Auth-implemenation to main

--- apps/mobile/src/config.ts
+++ apps/mobile/src/config.ts
@@ -2,6 +2,6 @@ import { Capacitor } from "@capacitor/core";
 
 export const config = {
   backendUrl: Capacitor.isNativePlatform()
-    ? `${process.env.VITE_MOBILE_LOCAL_URL}`
-    : `${process.env.VITE_BACKEND_URL}`,
+    ? import.meta.env.VITE_MOBILE_LOCAL_URL
+    : import.meta.env.VITE_BACKEND_URL,
 };
